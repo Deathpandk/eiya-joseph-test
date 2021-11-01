b3ecb310fb4d8a8eb78b486cf56e39e3d506cfdc
Joseph Sierra <joseph.sgtz@gmail.com>
2021-11-01 13:57:47 -0600

    Trip serializer

--- src/vehicles/serializers.py
+++ src/vehicles/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 
-from vehicles.models import Vehicle
+from vehicles.models import Trip, Vehicle
 
 class VehicleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Vehicle
-        fields = ("id", "current_location", "fuel_consumption", "distance_traveled", "fuel_consumed")
-        read_only_fields = ("distance_traveled", "fuel_consumed")
+        fields = ('id', 'current_location', 'fuel_consumption', 'distance_traveled', 'fuel_consumed')
+        read_only_fields = ('distance_traveled', 'fuel_consumed')
+
+class TripSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Trip
+        fields = ('pk', 'vehicle', 'origin', 'destiny', 'distance_traveled', 'created')
+        read_only_fields = ('distance_traveled', 'created')
+
+    def validate(self, attrs):
+        if attrs['origin'] != attrs['vehicle'].current_location:
+            raise serializers.ValidationError('Vehicle current location is different from trip origin')
+        return attrs
+
