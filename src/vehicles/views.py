51997833b1ec2eb73cbfe632084e22b962aae708
Joseph Sierra <joseph.sgtz@gmail.com>
2021-11-01 19:06:00 -0600

    create form in index

--- src/vehicles/views.py
+++ src/vehicles/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
 
+from cities.models import City
 
 class VehiclesIndex(TemplateView):
     template_name = 'vehicles/index.html'
+    extra_context = {'cities': City.objects.all()}
